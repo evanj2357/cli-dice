92f17c095e62e453625aad4b612851d32b4861b1
Evan Johnson <evanj2357@gmail.com>
2020-12-18 21:18:32 -0800

    clean-up

--- src/dice.rs
+++ src/dice.rs
@@ -3,7 +3,7 @@ use nom::{
     character::complete::{digit0, digit1},
     complete,
     error::Error,
-    map, map_res, named, opt, parse_to, terminated, try_parse, verify, Err, Finish, IResult,
+    map, map_res, opt, parse_to, terminated, verify, Finish, IResult,
 };
 use rusty_dice::*;
 use std::str::FromStr;
@@ -27,6 +27,8 @@ impl Dice {
         n_rolls(self.count, &self.die).iter().sum::<i32>() + self.modifier
     }
 
+    // TODO: implement "roll at advantage" feature to use this
+    #[allow(dead_code)]
     pub fn roll_n_take_highest(&self, n: usize) -> i32 {
         (0..n)
             .map(|_| *n_rolls(self.count, &self.die).iter().max().unwrap_or(&0) + self.modifier)
@@ -34,6 +36,8 @@ impl Dice {
             .unwrap_or(0)
     }
 
+    // TODO: implement "roll at disadvantage" feature to use this
+    #[allow(dead_code)]
     pub fn roll_n_take_lowest(&self, n: usize) -> i32 {
         (0..n)
             .map(|_| *n_rolls(self.count, &self.die).iter().min().unwrap_or(&0) + self.modifier)
@@ -58,7 +62,7 @@ impl FromStr for Dice {
 
 fn parse_dice_spec(s: &str) -> IResult<&str, (usize, i32, i32)> {
     let (i1, count) = terminated!(s, opt!(map_res!(digit0, |s1: &str| s1.parse())), char!('d'))?;
-    let (i2, max_die_val) = verify!(i1, positive_int, |&v| v > 0)?;
+    let (i2, max_die_val) = verify!(i1, map!(digit1, |s| s.parse().unwrap()), |&v| v > 0)?;
     let (i3, modifier) = opt!(i2, parse_to!(i32))?;
 
     let n_dice = if let Some(n) = count {
@@ -72,8 +76,3 @@ fn parse_dice_spec(s: &str) -> IResult<&str, (usize, i32, i32)> {
     };
     Ok((i3, (n_dice, max_die_val, modifier.unwrap_or(0))))
 }
-
-// parses only positive i32 values by accepting only digit characters
-named!(positive_int<&str, i32>, map!(digit1, |s| s.parse::<i32>().unwrap()));
-
-// named!(usize_or_0<&str, usize>, map!(digit0, |s| parse_to!(s, usize)));
